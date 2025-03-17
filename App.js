3a720b7791c0b8a69f6b22f6e4e82cb6692e58c4
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-17 22:15:53 +0530

    update App.js file

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ const itemRoutes = require("./routes/itemRoutes");
 require("dotenv").config();
 
 const app = express();
+
+// Connect to database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
 );
