2a51fc80565b24e262298dbb549a97937cac0f80
Shehar Mavitha <mavithashehar@gmail.com>
2025-03-18 19:10:07 +0530

    change user find out username to email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/Users");
@@ -8,7 +9,7 @@ const registerUser = async (req, res) => {
     try {
         const { username, email, password, role } = req.body;
 
-        const existingUser = await User.findOne({ username });
+        const existingUser = await User.findOne({ email });
         if (existingUser) return res.status(400).json({ message: "User already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -24,8 +25,8 @@ const registerUser = async (req, res) => {
 // Login User
 const loginUser = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
         if (!user || !(await bcrypt.compare(password, user.password))) {
             return res.status(400).json({ message: "Invalid credentials" });
